81ddbd4677871b6d874150bf13dea99b82a6609f
sundaramoorthy <sundaramoorthy.gopalakrishnan@whatfix.com>
2023-05-03 10:00:56 +0530

    Theme with props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Theme from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello Team KitKat</h1>  
+    <Theme />
   </React.StrictMode>
 );
