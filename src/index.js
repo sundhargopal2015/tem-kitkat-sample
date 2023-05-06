25e90ee661081f40582c2844a99a26877be38697
sundaramoorthy <sundaramoorthy.gopalakrishnan@whatfix.com>
2023-05-06 10:33:44 +0530

    code cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Theme from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Theme />
+    <h1>Hello React</h1>
   </React.StrictMode>
 );
