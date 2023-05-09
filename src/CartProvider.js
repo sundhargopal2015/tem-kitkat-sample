950f7d944d5c071d705be994a247277d78905d91
sundaramoorthy <sundaramoorthy.gopalakrishnan@whatfix.com>
2023-05-09 11:19:32 +0530

    Add to cart

--- src/CartProvider.js
+++ src/CartProvider.js
@@ -5,20 +5,50 @@ export const CartContext = createContext({
   addToOneCart: function () {},
   removeOneToCart: function () {},
   removeFromCart: function () {},
-  getProductQauntity: function () {},
-  getTotalcost: function () {},
+  getProductQuantity: function () {},
+  getTotalCost: function () {},
 });
 
 const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState[[{ id: 1, quantity: 1 }]];
-  
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToOneCart = (id) => {
+    setCartItems((pre) => {
+      let finalVal = [];
+      if (pre.length) {
+        const find = pre.find((cartItem) => cartItem.id === id);
+        if (find) {
+          finalVal = pre.map((cartItem) =>
+          cartItem.id === find.id
+              ? {
+                  id: find.id,
+                  quantity: find.quantity + 1,
+                }
+              : cartItem
+          );
+        } else {
+          finalVal = [...pre, { id: id, quantity: 1 }];
+        }
+      } else {
+        finalVal = [
+          {
+            id: id,
+            quantity: 1,
+          },
+        ];
+      }
+
+      return finalVal;
+    });
+  };
+
   const contextValue = {
     items: cartItems,
-    addToOneCart: function () {},
+    addToOneCart: addToOneCart,
     removeOneToCart: function () {},
     removeFromCart: function () {},
-    getProductQauntity: function () {},
-    getTotalcost: function () {},
+    getProductQuantity: function () {},
+    getTotalCost: function () {},
   };
   return (
     <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,31 @@
-import React from "react"
+import React, { useContext } from "react";
 import { Button, Card } from "react-bootstrap";
-import {CoinRupee} from "tabler-icons-react";
+import { CoinRupee } from "tabler-icons-react";
+import { CartContext } from "../CartProvider";
 
+const ProductCard = ({ product }) => {
+  const cartContext = useContext(CartContext);
 
-const ProductCard = ({product}) => {
-    return (
-        <Card>
-            <Card.Body>
-                <Card.Title>{product.name}</Card.Title>
-                <Card.Text><CoinRupee />{product.price}</Card.Text>
-                <Button variant="primary">Add to cart</Button>
-            </Card.Body>
-        </Card>
-    )
-}
+  const handleAddToCart = () => {
+    cartContext.addToOneCart(product.id);
+  };
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Text>
+          <CoinRupee />
+          {product.price}
+        </Card.Text>
+        <Button variant="primary" onClick={handleAddToCart}>
+          {cartContext.items.find((val) => val.id === product.id)
+            ? "+"
+            : "Add to cart"}
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default ProductCard;
